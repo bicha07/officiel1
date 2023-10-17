180d9cbd82396bf213bf17b7701ddfc743f05dcb
bicha07 <yessinegargouri07@gmail.com>
2023-10-17 16:02:15 +0100

    file python

---
+++ correctionBug.py
@@ -0,0 +1,10 @@
+def divide_numbers(a, b):
+if b == 0:
+return "Division by zero is not allowed."
+return a / b
+
+num1 = 10
+num2 = 0
+
+result = divide_numbers(num1, num2)
+print(f"Result of division: {result}")
