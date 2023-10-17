5ee90a6138b8a93ec190f16e5a1d2c6851911923
bicha07 <yessinegargouri07@gmail.com>
2023-10-17 16:07:49 +0100

    file python corriged

--- correctionBug.py
+++ correctionBug.py
@@ -1,10 +1,10 @@
-def divide_numbers(a, b):
-if b == 0:
-return "Division by zero is not allowed."
-return a / b
+def diviser_nombres(a, b):
+    if b == 0:
+        return "Division par zéro n'est pas autorisée."
+    return a / b
 
 num1 = 10
 num2 = 0
 
-result = divide_numbers(num1, num2)
-print(f"Result of division: {result}")
+resultat = diviser_nombres(num1, num2)
+print(f"Résultat de la division : {resultat}")
